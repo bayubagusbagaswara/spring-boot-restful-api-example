53846c2670c4d4ce841a32a303613995053624d6
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-20 10:09:34 +0700

    rename main application

--- src/main/java/com/bbb/restfulapiexample/RestfulApiExampleApplication.java
+++ src/main/java/com/bbb/restfulapiexample/RestfulApiExampleApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.bbb.restfulapiexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class RestfulApiExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(RestfulApiExampleApplication.class, args);
 	}
 
 }
