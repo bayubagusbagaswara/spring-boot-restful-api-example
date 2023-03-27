ddc4afb445695f1e62fbc273204176cf4ac9e54e
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-27 08:35:06 +0700

    add method findProblemByProjectCode

--- src/main/java/com/bbb/restfulapiexample/repository/ProblemRepository.java
+++ src/main/java/com/bbb/restfulapiexample/repository/ProblemRepository.java
@@ -2,10 +2,16 @@ package com.bbb.restfulapiexample.repository;
 
 import com.bbb.restfulapiexample.model.Problem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProblemRepository extends JpaRepository<Problem, Long> {
+
+    @Query(value = "SELECT * FROM Problem p WHERE p.project.code = ?1", nativeQuery = true)
+    List<Problem> findProblemByProjectCode(String code);
 
 
 }
